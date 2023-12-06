900eeb7463ce70eeda216e0b37fcb25785853b31
GlennVinck <vinck_glenn@hotmail.com>
2023-12-06 16:48:47 +0100

    feat: display plane for ground

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 import "./style.css";
 import * as THREE from "three";
 // import GLTFLoader for loading models
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 // import controls
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 // import dat.gui
-import * as dat from 'dat.gui';
+import * as dat from "dat.gui";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -14,24 +14,37 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
+camera.position.z = 5;
+scene.add(camera);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
 
+const groundGeometry = new THREE.PlaneGeometry(10, 10);
+const groundmaterial = new THREE.MeshBasicMaterial({
+  color: 0xffffff,
+  side: THREE.DoubleSide,
+});
+const plane = new THREE.Mesh(groundGeometry, groundmaterial);
+plane.rotation.x = Math.PI / 2;
+plane.position.y = -0.5;
+plane.receiveShadow = true;
+plane.castShadow = true;
+scene.add(plane);
 
-const cubeTextureLoader = new THREE.CubeTextureLoader();
-const environmentMapTexture = cubeTextureLoader.load([
-  "/cubemap/px.png",
-  "/cubemap/nx.png",
-  "/cubemap/py.png",
-  "/cubemap/ny.png",
-  "/cubemap/pz.png",
-  "/cubemap/nz.png",
-]);
+// const cubeTextureLoader = new THREE.CubeTextureLoader();zzzz
+// const environmentMapTexture = cubeTextureLoader.load([
+//   "/cubemap/px.png",
+//   "/cubemap/nx.png",
+//   "/cubemap/py.png",
+//   "/cubemap/ny.png",
+//   "/cubemap/pz.png",
+//   "/cubemap/nz.png",
+// ]);
 
-scene.background = environmentMapTexture;
+//scene.background = environmentMapTexture;
 
 // add controls
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -51,18 +64,14 @@ const componentColorOptions = {
 
 function setupGUI(mesh, material, component) {
   gui = new dat.GUI();
-  const folder = gui.addFolder(mesh.name || 'Mesh');
+  const folder = gui.addFolder(mesh.name || "Mesh");
   folder.open();
 }
 
-
 // load models/biker_boot_model.glb
-gltfLoader.load('/models/shoe-optimized-arne.glb', (gltfBiker) => {
+gltfLoader.load("/models/shoe-optimized-arne.glb", (gltfBiker) => {
   const bikerBoot = gltfBiker.scene.children[0];
   console.log(bikerBoot); // Log the loaded model to inspect its properties
-
-
-
 
   let outsideSole = new THREE.MeshStandardMaterial({
     color: 0xffff00, // Red
@@ -76,7 +85,6 @@ gltfLoader.load('/models/shoe-optimized-arne.glb', (gltfBiker) => {
     roughness: 0.4,
   });
 
-
   let inside = new THREE.MeshStandardMaterial({
     color: 0x800080,
     metalness: 0.7,
@@ -88,25 +96,24 @@ gltfLoader.load('/models/shoe-optimized-arne.glb', (gltfBiker) => {
     metalness: 0.7,
     roughness: 0.4,
   });
-  
+
   let laces = new THREE.MeshStandardMaterial({
     color: 0x00ff00, // SaddleBrown (you can change the color as needed)
     metalness: 0.7,
     roughness: 0.4,
   });
-  
+
   let strap = new THREE.MeshStandardMaterial({
     color: 0x00ff00, // DarkOrchid (you can change the color as needed)
     metalness: 0.7,
     roughness: 0.4,
   });
 
-let defaultMaterial = new THREE.MeshStandardMaterial({
-  color: 0xff0000,
-  metalness: 0.7,
-  roughness: 0.4,
-});
-  
+  let defaultMaterial = new THREE.MeshStandardMaterial({
+    color: 0xff0000,
+    metalness: 0.7,
+    roughness: 0.4,
+  });
 
   // adjust position and scale of the biker boot model
   bikerBoot.position.set(0, 0, 0); // Adjust the position as needed
@@ -115,17 +122,16 @@ let defaultMaterial = new THREE.MeshStandardMaterial({
   bikerBoot.traverse((child) => {
     if (child.isMesh) {
       child.material = defaultMaterial;
-    child.castShadow = true;
-    child.receiveShadow = true;
+      child.castShadow = true;
+      child.receiveShadow = true;
 
-    
       switch (child.name) {
         case "sole_1":
-          child.material = insideSole; 
+          child.material = insideSole;
           break;
 
         case "sole_2":
-          child.material = outsideSole; 
+          child.material = outsideSole;
           break;
 
         case "outside_2":
@@ -137,48 +143,48 @@ let defaultMaterial = new THREE.MeshStandardMaterial({
           break;
 
         case "laces":
-          child.material = laces; 
+          child.material = laces;
           break;
 
         case "inside":
-          child.material = inside; 
+          child.material = inside;
           break;
       }
 
-      
-
       child.castShadow = true;
       child.receiveShadow = true;
-  }
+    }
   });
 
   scene.add(gltfBiker.scene);
   findObjectsByName(gltfBiker.scene, "");
 });
 
-  // Add lights
-  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-  scene.add(ambientLight);
+// Add lights
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
 
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
-  directionalLight.position.set(-550, 60, 600);
-  directionalLight.castShadow = true;
-  scene.add(directionalLight);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+directionalLight.position.set(0, 2, 2);
+directionalLight.castShadow = true;
 
-  const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 1);
-  scene.add(directionalLightHelper);
+scene.add(directionalLight);
 
-  // shadows
-  directionalLight.shadow.mapSize.width = 1024;
-  directionalLight.shadow.mapSize.height = 1024;
-  directionalLight.shadow.camera.near = 1;
-  directionalLight.shadow.camera.far = 500;
+const directionalLightHelper = new THREE.DirectionalLightHelper(
+  directionalLight,
+  1
+);
+scene.add(directionalLightHelper);
 
+// shadows
+directionalLight.shadow.mapSize.width = 1024;
+directionalLight.shadow.mapSize.height = 1024;
+directionalLight.shadow.camera.near = 1;
+directionalLight.shadow.camera.far = 500;
 
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.shadowMap.autoUpdate = true;
 renderer.shadowMap.needsUpdate = true;
-
 
 // make window resize responsive
 function onWindowResize() {
@@ -189,14 +195,9 @@ function onWindowResize() {
 
 window.addEventListener("resize", onWindowResize);
 
-
-camera.position.set(0, 15, 30);
-// camera.position.z = 80;
-
 function animate() {
   requestAnimationFrame(animate);
   controls.update();
-
 
   renderer.render(scene, camera);
 }
