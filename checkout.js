153bd293600c0e4052ec58ffbf0260192faa25ae
Wolf <wolfverelst@gmail.com>
2023-12-16 21:30:59 +0100

    feat(checkout): integrate order submission functionality

--- checkout.js
+++ checkout.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+    const customerName = "Joris Hens";
+    const email = "joris@hens.be";
+    const price = 175.00;
+
     // retrieve order data from local storage
     const orderDataString = localStorage.getItem("orderData");
     const orderData = JSON.parse(orderDataString);
@@ -36,6 +41,49 @@ document.addEventListener("DOMContentLoaded", function () {
             console.error("Element with ID 'price' not found in the document.");
         }
 
+        const customerNameElement = document.getElementById("customerName");
+        if (customerNameElement) {
+            customerNameElement.textContent = orderData.customerName;
+        } else {
+            console.error("Element with ID 'customerName' not found in the document.");
+        }
+
+        const emailElement = document.getElementById("email");
+        if (emailElement) {
+            emailElement.textContent = orderData.email;
+        } else {
+            console.error("Element with ID 'email' not found in the document.");
+        }
+
+        const orderNumberElement = document.getElementById("orderNumber");
+        if (orderNumberElement) {
+            // generate a random order number
+            orderData.orderNumber = Math.floor(Math.random() * 1000000000);
+            orderNumberElement.textContent = orderData.orderNumber;
+        } else {
+            console.error("Element with ID 'orderNumber' not found in the document.");
+        }
+
+        const deliveryAddressElement = document.getElementById("deliveryAddress");
+        if (deliveryAddressElement) {
+            // generate a random delivery address
+            orderData.deliveryAddress = `${Math.floor(Math.random() * 1000)} Main Street`;
+            deliveryAddressElement.textContent = orderData.deliveryAddress;
+        } else {
+            console.error("Element with ID 'deliveryAddress' not found in the document.");
+        }
+
+        const orderDateElement = document.getElementById("orderDate");
+        if (orderDateElement) {
+            // generate a random order date
+            orderData.orderDate = new Date().toLocaleDateString();
+            orderDateElement.textContent = orderData.orderDate;
+        } else {
+            console.error("Element with ID 'orderDate' not found in the document.");
+        }
+
+
+
         // display order details in the DOM
         const orderOverview = document.getElementById("snapshotImage");
         if (orderOverview) {
@@ -58,31 +106,40 @@ document.addEventListener("DOMContentLoaded", function () {
         console.error("Order data not found in local storage.");
     }
 
+    
+
+    const requestBody = {
+        customerName: customerName,
+        deliveryAdress: orderData.deliveryAddress,
+        email: email,
+        orderNumber: orderData.orderNumber,
+        orderDate: orderData.orderDate,
+        shoeSize: orderData.shoeSize,
+        price: price,
+        image: orderData.image,
+        quantity: orderData.quantity,
+        status: "pending"
+    };
+
     // add event listener for the "BUY NOW" button
-    // document.getElementById("buyBtn").addEventListener("click", function () {
-    //     // send order data to the server
-    //   fetch("your-api-endpoint", {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify(orderData),
-    //   })
-    //     .then((response) => response.json())
-    //     .then((data) => {
-    //       // Handle the response from the server
-    //       console.log("Order placed successfully:", data);
-    //     })
-    //     .catch((error) => {
-    //       console.error("Error placing order:", error);
-    //     });
-    // });
-  });
+    document.getElementById("buyBtn").addEventListener("click", function () {
 
-  const colorLog = JSON.parse(localStorage.getItem("colorLog"));
-
-  // Log colors on the checkout page
-  console.log("Colors of selected objects (checkout):");
-  Object.entries(colorLog).forEach(([objectName, colorHex]) => {
-      console.log(`${objectName}: ${colorHex}`);
+        console.log("Order data:", requestBody);
+        // send order data to the server
+      fetch("http://localhost:3000/api/v1/orders", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestBody),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          // Handle the response from the server
+          console.log("Order placed successfully:", data);
+        })
+        .catch((error) => {
+          console.log("Error:", error);
+        });
+    });
   });
